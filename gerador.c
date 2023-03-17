74e4ead510994f6fabc31d8e06aa014f3f99764e
Filipe <69159895+Curvu@users.noreply.github.com>
2023-03-17 20:16:52 +0000

    Add files via upload

--- gerador.c
+++ gerador.c
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define INPUT "inp.txt"
+#define OUTPUT "out.txt"
+#define MAX 100
+
 // generate a random_matricula : A-Z, 0-9, 6 characters
 char *generate_matricula() {
     char *chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -29,11 +33,60 @@ char *generate_infracao() {
     return infracoes[rand() % 5];
 }
 
-int main(int argc, char* argv[]) {
+void sortFile(int dim, int flag) {
+    system("./main 0 1 < inp.txt > out.txt"); // 0 = without time, 1 = merge sort
+    system("exec 1>&-"); // stop printing to out.txt
+    FILE *fp = fopen(INPUT, "w");
+    FILE *fp2 = fopen(OUTPUT, "r");
+    // ignore first and last line
+    char input[MAX];
+    fgets(input, MAX, fp2);
+    fprintf(fp, "DIM_BD %d\n", dim);
+    while (fgets(input, MAX, fp2) != NULL) {
+        if (strcmp(input, "FIM\n") == 0) break;
+        fprintf(fp, "%s", input);
+    }
+    if (flag == 1) fprintf(fp, "CONSULTA_BD\n");
+    fprintf(fp, "TCHAU\n");
+    fclose(fp);
+    fclose(fp2);
+}
+
+int main(int argc, char** argv) { // ./gerar <dim> <flag {0 = random, 1 = sorted, 2 = reversed}>
+    int dim = atoi(argv[1]), flag = atoi(argv[2]);
     srand(time(NULL));
-    int dim = atoi(argv[1]);
-    printf("DIM_BD %d\n", dim);
-    for (int i = 0; i < dim; i++) printf("%s %s %d %s\n", generate_matricula(), generate_infracao(), generate_num(), generate_name());
-    printf("TCHAU");
+
+    FILE *fp = fopen(INPUT, "w");
+    fprintf(fp, "DIM_BD %d\n", dim);
+    for (int i = 0; i < dim; i++) fprintf(fp, "%s %s %d %s\n", generate_matricula(), generate_infracao(), generate_num(), generate_name());
+    if (flag == 1 || flag == 2) {
+        fprintf(fp, "CONSULTA_BD\n");
+        FILE *fp2 = fopen(OUTPUT, "w");
+        fclose(fp2);
+    }
+    fprintf(fp, "TCHAU\n");
+    fclose(fp);
+
+    if (flag == 1) { // sort
+        sortFile(dim, 0);
+    } else if (flag == 2) { // decrescente
+        sortFile(dim, 1);
+
+        system("./main 0 3 < inp.txt > out.txt"); // 0 = without time, 3 = reverse
+        system("exec 1>&-"); // stop printing to out.txt
+        FILE *fp = fopen(INPUT, "w");
+        FILE *fp2 = fopen(OUTPUT, "r");
+        // ignore first and last line
+        char input[MAX];
+        fgets(input, MAX, fp2);
+        fprintf(fp, "DIM_BD %d\n", dim);
+        while (fgets(input, MAX, fp2) != NULL) {
+            if (strcmp(input, "FIM\n") == 0) break;
+            fprintf(fp, "%s", input);
+        }
+        fprintf(fp, "TCHAU\n");
+        fclose(fp);
+        fclose(fp2);
+    }
     return 0;
 }
